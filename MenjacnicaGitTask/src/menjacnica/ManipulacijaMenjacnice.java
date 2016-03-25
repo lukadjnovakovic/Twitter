616bf111914f8e2c5ce0d78c182cd2e9f903a046
lukadjnovakovic <lukadjnovakovic@gmail.com>
2016-03-25 17:04:24 +0100

    napravljena nova klasa koja  implementira InterfejsMenjacnice.

---
+++ MenjacnicaGitTask/src/menjacnica/ManipulacijaMenjacnice.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import interfejs.InterfaceMenjacnice;
+
+public class ManipulacijaMenjacnice implements InterfaceMenjacnice {
+
+	@Override
+	public void dodajKurs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double pronadjiKurs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
