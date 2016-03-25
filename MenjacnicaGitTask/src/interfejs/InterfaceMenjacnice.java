1678d6c084e98246a52152373bf62c6534c5542d
lukadjnovakovic <lukadjnovakovic@gmail.com>
2016-03-25 17:25:52 +0100

    implementirana metoda dodajKurs na master grani

--- MenjacnicaGitTask/src/interfejs/InterfaceMenjacnice.java
+++ MenjacnicaGitTask/src/interfejs/InterfaceMenjacnice.java
@@ -1,10 +1,14 @@
 package interfejs;
 
+import java.util.LinkedList;
+
+import menjacnica.valute.Valuta;
+
 public interface InterfaceMenjacnice {
 	
-	public void dodajKurs();
-	public void obrisiKurs();
-	public double pronadjiKurs();
+	public void dodajKurs(double a,double b,double c);
+	public void obrisiKurs(double a,double b,double c,LinkedList<Valuta> valute);
+	public double pronadjiKurs(double a,double b,double c,LinkedList<Valuta> valute);
 	
 	
 }
--- MenjacnicaGitTask/src/menjacnica/ManipulacijaMenjacnice.java
+++ MenjacnicaGitTask/src/menjacnica/ManipulacijaMenjacnice.java
@@ -1,23 +1,30 @@
 package menjacnica;
 
+import java.util.LinkedList;
+
 import interfejs.InterfaceMenjacnice;
+import menjacnica.valute.Valuta;
 
 public class ManipulacijaMenjacnice implements InterfaceMenjacnice {
 
 	@Override
-	public void dodajKurs() {
+	public void dodajKurs(double a,double b,double c) {
+		Valuta nova = new Valuta();
+		nova.setNaziv("nepoznato");
+		nova.setSkraceniNaziv("nepoznato");
+		nova.setKupovniKurs(a);
+		nova.setProdajniKurs(b);
+		nova.setSrednjiKurs(c);
+	}
+
+	@Override
+	public void obrisiKurs(double a,double b,double c,LinkedList<Valuta> valute) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void obrisiKurs() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double pronadjiKurs() {
+	public double pronadjiKurs(double a,double b,double c,LinkedList<Valuta> valute) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
